97da6a3872d5fbc671f3d398e957cc07bc632271
MoniqueTheogene123 <716theogene@gmail.com>
2026-02-12 17:12:48 -0600

    Created Projects table and SQL

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2026_02_12_230853) do
+ActiveRecord::Schema[8.0].define(version: 2026_02_12_231205) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
+
+  create_table "projects", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "skills", force: :cascade do |t|
     t.string "name"
