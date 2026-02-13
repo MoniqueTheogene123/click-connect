8fdbbbc82b3be6293686e8217dfc8e35a20e853b
MoniqueTheogene123 <716theogene@gmail.com>
2026-02-13 17:44:14 -0600

    commit skill.rb

--- app/models/skill.rb
+++ app/models/skill.rb
@@ -8,8 +8,8 @@
 #  updated_at :datetime         not null
 #
 class Skill < ApplicationRecord
-  belongs_to :user
-  
+
+
   has_many :user_skills, class_name: "UserSkill", foreign_key: "skill_id", dependent: :destroy
   has_many :users, through: :user_skills, source: :user
 end
