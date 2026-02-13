d9340a4be1c058a85f533d01d9ded6787852698c
MoniqueTheogene123 <716theogene@gmail.com>
2026-02-13 13:16:56 -0600

    debug homepage and dashboard pages

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,9 +1,8 @@
 class PagesController < ApplicationController
   def home
-    if user_signed_in?
-      redirect_to dashboard_index_path
-    else
+    # if user_signed_in?
+    #   redirect_to dashboard_url
+    # else
       render :home
     end 
   end
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,14 @@
 Rails.application.routes.draw do
-  get "/dashboard", to: "dashboard#index"
+  root to: "pages#home"
   get "pages/home"
+  get "/dashboard", to: "dashboard#index"
+
   resources :user_projects
   resources :projects
   resources :skills
   devise_for :users
 
-  root to: "pages#home"
+
 
   # This is a blank app! Pick your first screen, build out the RCAV, and go from there. E.g.:
   # get("/your_first_screen", { :controller => "pages", :action => "first" })
