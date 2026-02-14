0f41a1969c6c9d8122594805d26676b760df4e16
MoniqueTheogene123 <716theogene@gmail.com>
2026-02-14 10:50:18 -0600

    bug fix

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,17 @@
 Rails.application.routes.draw do
-  resources :user_skills
   root to: "pages#home"
   get "pages/home"
   get "/dashboard", to: "dashboard#index"
-
+  
+  resources :user_skills
   resources :user_projects
   resources :projects
   resources :skills
   devise_for :users
 
-
-get("/", { :controller => "pages", :action => "home" })
-get("/index", { :controller => "pages", :action => "index" })
-get("/dashboard", { :controller => "dashboard", :action => "index" })
+  get("/", { :controller => "pages", :action => "home" })
+  get("/index", { :controller => "pages", :action => "index" })
+  get("/dashboard", { :controller => "dashboard", :action => "index" })
   # This is a blank app! Pick your first screen, build out the RCAV, and go from there. E.g.:
   # get("/your_first_screen", { :controller => "pages", :action => "first" })
 end
