72961eda3114e8ac58c9128740adf8f051e6bd41
MoniqueTheogene123 <716theogene@gmail.com>
2026-02-18 11:23:23 -0600

    Created profiles controller and setup RVAC

--- config/routes.rb
+++ config/routes.rb
@@ -6,12 +6,16 @@ Rails.application.routes.draw do
   resources :user_projects
   resources :projects
   resources :skills
+  resources :profiles
   devise_for :users
 
   get("/", { :controller => "pages", :action => "home" })
   get("/index", { :controller => "pages", :action => "index" })
+
   get("/dashboard", { :controller => "dashboard", :action => "index" })
   get("/dashboard/:user", { :controller => "dashboard", :action => "index" })
+  
+  get("/profiles", { :controller => "profiles", :action => "index" })
 
   # This is a blank app! Pick your first screen, build out the RCAV, and go from there. E.g.:
   # get("/your_first_screen", { :controller => "pages", :action => "first" })
