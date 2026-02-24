ab7b87270d2a2c8c8009fa70128972535bedf2ff
MoniqueTheogene123 <716theogene@gmail.com>
2026-02-24 16:29:25 -0600

    route updated

--- config/routes.rb
+++ config/routes.rb
@@ -16,9 +16,6 @@ Rails.application.routes.draw do
   # Devise routes
   devise_for :users
 
-  # Pages routes
-  get "/index", to: "pages#index"
-
   # This is a blank app! Pick your first screen, build out the RCAV, and go from there. E.g.:
   # get("/your_first_screen", { :controller => "pages", :action => "first" })
 end
